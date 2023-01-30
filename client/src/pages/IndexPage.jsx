ce2609a693ef07ce44dc9e8decbfc1c2ee11fde4
ualiu <urim.aliu@gmail.com>
2023-01-29 20:34:48 -0500

    first commit

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import Header from "../Header";
+
+export default function IndexPage() {
+  return (
+    <div>
+      INDEX PAGE HERE
+    </div>
+  );
+}
+
