09f60b9545ae82ceebb7250a4090974d5b2c6ff3
ualiu <urim.aliu@gmail.com>
2023-02-02 20:51:47 -0500

    added places pages and form to upload info

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
-const CookieParser = require('cookie-parser');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require ('image-downloader');
 require('dotenv').config()
 const app = express();
 
@@ -14,6 +14,7 @@ const jwtSecret = 'sd;gkjasdg984qh3wgv';
 
 app.use(express.json())
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
   origin: 'http://localhost:5173',
@@ -76,6 +77,16 @@ app.get('/profile', (req,res) => {
 
 app.post('/logout', (req,res) => {
   res.cookie('token', '').json(true);
+})
+
+app.post('/upload-by-link', async (req,res) => {
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + '/uploads' +newName,
+  });
+  res.json(newName)
 })
 
 app.listen(4000);
