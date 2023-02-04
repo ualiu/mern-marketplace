ded4edb5ab22b5f26dab69012f713f09354a1a82
ualiu <urim.aliu@gmail.com>
2023-02-03 21:38:49 -0500

    updated perks checkboxes

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require ('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config()
 const app = express();
 
@@ -87,6 +90,20 @@ app.post('/upload-by-link', async (req,res) => {
     dest: __dirname + '/uploads' +newName,
   });
   res.json(newName)
+})
+
+const photosMiddleware = multer({dest: 'uploads/'});
+app.post('/upload', photosMiddleware.array('photos', 100), (req,res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i <req.files.length; i++) {
+    const {path, originalname} = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace('uploads', ''));
+  }
+  res.json(uploadedFiles);
 })
 
 app.listen(4000);
